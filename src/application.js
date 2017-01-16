6bb71611f071bf40c4966cb99a6371446418d6aa
edapx <lastexxit@gmail.com>
2017-01-16 23:21:10 +0100

    add data gui

--- src/application.js
+++ src/application.js
@@ -1,9 +1,18 @@
 /* eslint-env browser */
 import {Scene, PerspectiveCamera, WebGLRenderer} from 'three';
 import Cube from './cube.js';
+import DAT from 'dat-gui';
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const gui = new DAT.GUI();
+var params = {
+    pos_x: 0,
+    pos_y: 0
+};
+
+gui.add(params, "pos_x").min(-10).max(10).step(1);
+gui.add(params, "pos_y").min(-10).max(10).step(1);
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,7 +24,8 @@ camera.position.z = 5;
 
 function render(){
 	requestAnimationFrame(render);
-	cube.update();
+    cube.mesh.position.x = params.pos_x;
+    cube.mesh.position.y = params.pos_y;
 	renderer.render(scene, camera);
 }
 
